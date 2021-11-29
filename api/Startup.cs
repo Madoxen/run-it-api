056d42c0c08ab8a23c6433b3eff079f66b906c23
Paweł Chłąd <pchlad5@gmail.com>
2021-11-29 18:44:44 +0100

    Added service to the DI

--- api/Startup.cs
+++ api/Startup.cs
@@ -80,6 +80,7 @@ namespace Api
             });
 
             services.AddSingleton<IAuthorizationHandler, UserAuthorizationHandler>();
+            services.AddScoped<IUserAuthService, UserAuthService>();
             services.AddScoped<IUserService, UserService>();
             services.AddScoped<IFriendService, FriendService>();
             services.AddScoped<IFriendRequestService, FriendRequestService>();
