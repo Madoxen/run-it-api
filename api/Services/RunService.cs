59afd5403fab2ca9129a42a6b72730cbb52e1056
Paweł Chłąd <pchlad5@gmail.com>
2021-12-02 19:25:54 +0100

    Added run controller (#15)

---
+++ api/Services/RunService.cs
@@ -0,0 +1,78 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Api.Models;
+using Api.Utils;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Services
+{
+    public interface IRunService
+    {
+        Task<Run> GetRunById(int id);
+        Task<ServiceResult<List<Run>>> GetUserRuns(int userId);
+        Task<ServiceResult> RemoveRunById(int id);
+        Task<ServiceResult> RemoveRun(Run u);
+        Task<ServiceResult> UpdateRun(Run u);
+        Task CreateRun(Run u);
+    }
+
+    public class RunService : ServiceBase, IRunService
+    {
+        public readonly ApiContext _context;
+        public RunService(ApiContext context)
+        {
+            _context = context;
+        }
+
+        public async Task CreateRun(Run u)
+        {
+            _context.Runs.Add(u);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<Run> GetRunById(int id)
+        {
+            var result = await _context.Runs.FindAsync(id);
+            return result;
+        }
+
+        public async Task<ServiceResult<List<Run>>> GetUserRuns(int userId)
+        {
+            var users = _context.Users
+            .Include(x => x.Runs);
+
+            var user = await users.FirstOrDefaultAsync(x => x.Id == userId);
+            if (user == null)
+                return NotFound("User not found");
+            return user.Runs;
+        }
+
+        public async Task<ServiceResult> RemoveRun(Run u)
+        {
+            _context.Runs.Remove(u);
+            await _context.SaveChangesAsync();
+            return Success();
+        }
+
+        public async Task<ServiceResult> RemoveRunById(int id)
+        {
+            Run run = await _context.Runs.FindAsync(id);
+            if (run == null)
+                return NotFound("User not found");
+            _context.Runs.Remove(run);
+            await _context.SaveChangesAsync();
+            return Success();
+        }
+
+        public async Task<ServiceResult> UpdateRun(Run u)
+        {
+            Run user = await _context.Runs.FindAsync(u.Id);
+            if (user == null)
+                return NotFound("User not found");
+            _context.Runs.Update(user);
+            await _context.SaveChangesAsync();
+            return Success();
+        }
+    }
+
+}
---
+++ tests/RunControllerUnitTests.cs
@@ -0,0 +1,274 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Api.Models;
+using Xunit;
+using Api.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.DependencyInjection;
+using Api.Handlers;
+using Api.Tests.Utils;
+using Microsoft.Extensions.Options;
+using Api.Configuration.Options;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
+using Api.Tests.Mocks;
+using Microsoft.EntityFrameworkCore;
+using System;
+using Api.Services;
+
+namespace Api.Tests
+{
+    public class RunControllerUnitTests
+    {
+        public RunControllerUnitTests()
+        {
+            List<Run> runs = new List<Run>();
+            var run = new Run()
+            {
+                Id = 1,
+            };
+            runs.Add(run);
+
+            List<User> users = new List<User>();
+            var user = new User()
+            {
+                Id = 1,
+                Weight = 1,
+                Runs = runs
+            };
+            users.Add(user);
+
+            _runService = new MockRunService(runs, users);
+        }
+
+        private IRunService _runService { get; set; }
+
+        private enum UserAuthorizationHandlerMode
+        {
+            SUCC = 0,
+            FAIL = 1,
+        }
+
+        private IAuthorizationService ArrangeAuthService(
+            IAuthorizationService authService = null,
+            UserAuthorizationHandlerMode handlerType = UserAuthorizationHandlerMode.SUCC)
+        {
+
+            //Arrange Authentication context
+            IOptions<AuthenticationOptions> options = Options.Create(new AuthenticationOptions()
+            {
+                JWT = new JWTOptions()
+                {
+                    Audience = "runit.co",
+                    Issuer = "runit.co",
+                    ExpiryMinutes = 20,
+                    Key = "aaabbbcccdddeeefffggghhhiiijjjkkk"
+                }
+            });
+
+            if (authService == null)
+            {
+                //Create authorization service
+                authService = Builders.BuildAuthorizationService(services =>
+                {
+                    if (handlerType == UserAuthorizationHandlerMode.SUCC)
+                        services.AddScoped<IAuthorizationHandler, MockUserAuthorizationHandlerSuccess>();
+
+                    if (handlerType == UserAuthorizationHandlerMode.FAIL)
+                        services.AddScoped<IAuthorizationHandler, MockUserAuthorizationHandlerFail>();
+
+                    services.AddAuthorization(options =>
+                    {
+                        options.AddPolicy("CheckUserIDResourceAccess", policy => policy.Requirements.Add(new SameUserIDRequirement()));
+                    });
+                });
+            }
+
+            return authService;
+        }
+
+        private ControllerContext ArrangeControllerContext(List<Claim> claims = null)
+        {
+            if (claims == null)
+            {
+                claims = new List<Claim>()
+                {
+                    new Claim("sub", "1"),
+                };
+            }
+
+            var identity = new ClaimsIdentity(claims);
+            var user = new ClaimsPrincipal(identity);
+
+            //Create controller context and supply it with security details
+            var context = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext
+                {
+                    User = user,
+                }
+            };
+
+            return context;
+        }
+
+        private RunController CreateDefaultTestController(IRunService runService, UserAuthorizationHandlerMode authMode = UserAuthorizationHandlerMode.SUCC)
+        {
+            RunController controller = new RunController(runService, ArrangeAuthService(handlerType: authMode));
+            controller.ControllerContext = ArrangeControllerContext();
+            return controller;
+        }
+
+        [Fact]
+        public async void TestGetEndpointWithExistingID()
+        {
+
+            //Arrange
+            RunController controller = CreateDefaultTestController(_runService);
+
+            //Act
+            ActionResult<List<Run>> result = await controller.Get(1);
+
+            //Assert
+            Assert.IsType<List<Run>>(result.Value);
+
+        }
+
+
+        [Fact]
+        public async void TestGetEndpointWithNonExistingID()
+        {
+
+            //Arrange
+            RunController controller = CreateDefaultTestController(_runService);
+
+            //Act
+            ActionResult<List<Run>> result = await controller.Get(2);
+
+            //Assert
+            Assert.IsType<NotFoundObjectResult>(result.Result);
+            Assert.Null(result.Value);
+
+        }
+
+        [Fact]
+        public async void TestDeleteEndpointWithExistingID()
+        {
+            //Arrange
+            RunController controller = CreateDefaultTestController(_runService);
+
+            //Act
+            var result = await controller.Delete(1);
+
+            //Assert
+            Assert.Null(await _runService.GetRunById(1));
+            Assert.IsType<OkResult>(result);
+        }
+
+
+        [Fact]
+        public async void TestDeleteEndpointWithNonExistingID()
+        {
+
+            //Arrange
+            RunController controller = CreateDefaultTestController(_runService);
+
+            //Act
+            var result = await controller.Delete(2);
+
+            //Assert
+            Assert.IsType<NotFoundObjectResult>(result);
+
+        }
+
+        [Fact]
+        public async void TestUpdateEndpointWithExistingID()
+        {
+
+            //Arrange
+            RunController controller = CreateDefaultTestController(_runService);
+
+            //Act
+            var result = await controller.Put(new Payloads.RunUpdatePayload()
+            {
+                Id = 1,
+                Duration = 1
+            });
+
+            //Assert
+            var result_service = await _runService.GetRunById(1);
+            Assert.IsType<OkResult>(result);
+            Assert.Equal(1u, result_service.Duration);
+        }
+
+
+        [Fact]
+        public async void TestUpdateEndpointWithNonExistingID()
+        {
+
+            //Arrange
+            RunController controller = CreateDefaultTestController(_runService);
+
+            //Act
+            var result = await controller.Put(new Payloads.RunUpdatePayload()
+            {
+                Id = 2,
+                Duration = 1
+            });
+
+            //Assert
+            var contextResult = await _runService.GetRunById(1);
+            Assert.IsType<NotFoundObjectResult>(result);
+            Assert.Equal(0u, contextResult.Duration);
+        }
+
+        [Fact]
+        public async void TestGetUnauthorized()
+        {
+            //Arrange
+            RunController controller = CreateDefaultTestController(_runService, UserAuthorizationHandlerMode.FAIL);
+
+            //Act
+            var result = await controller.Get(1);
+
+            //Assert
+            Assert.IsType<UnauthorizedResult>(result.Result);
+            Assert.Null(result.Value);
+
+        }
+
+        [Fact]
+        public async void TestUpdateUnauthorized()
+        {
+            //Arrange
+            RunController controller = CreateDefaultTestController(_runService, UserAuthorizationHandlerMode.FAIL);
+
+            //Act
+            var result = await controller.Put(new Payloads.RunUpdatePayload()
+            {
+                Id = 1,
+                Duration = 1
+            });
+
+            //Assert
+            var contextResult = await _runService.GetRunById(1);
+            Assert.IsType<UnauthorizedResult>(result);
+            Assert.Equal(0u, contextResult.Duration);
+        }
+
+        [Fact]
+        public async void TestDeleteUnauthorized()
+        {
+
+            //Arrange
+            RunController controller = CreateDefaultTestController(_runService, UserAuthorizationHandlerMode.FAIL);
+            //Act
+            var result = await controller.Delete(1);
+            //Assert
+            var contextResult = await _runService.GetRunById(1);
+            Assert.IsType<UnauthorizedResult>(result);
+            Assert.NotNull(contextResult);
+        }
+    }
+}
