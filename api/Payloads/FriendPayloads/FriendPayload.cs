26a531d7e0a7b96a672935ba5945a6325b33268e
Paweł Chłąd <pchlad5@gmail.com>
2021-12-09 18:25:32 +0100

    added additional info

--- api/Payloads/FriendPayloads/FriendPayload.cs
+++ api/Payloads/FriendPayloads/FriendPayload.cs
@@ -2,13 +2,22 @@ using Api.Models;
 
 namespace Api.Payloads
 {
-    public class FriendPayload 
+    public class FriendPayload : IModelPayload<User>
     {
         public int Id { get; set; }
+        public string GivenName { get; set; }
+        public string LastName { get; set; }
 
         public FriendPayload(User model)
         {
             Id = model.Id;
+            GivenName = model.GivenName;
+            LastName = model.LastName;
+        }
+
+        public User CreateModel()
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
