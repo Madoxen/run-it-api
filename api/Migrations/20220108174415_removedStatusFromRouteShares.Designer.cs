5dd38438c10f6bdadc79f127a718084e2c597dd4
Madoxen <pchlad5@gmail.com>
2022-01-08 18:44:41 +0100

    Migration:Removed status field

--- api/Migrations/20220108174415_removedStatusFromRouteShares.Designer.cs
+++ api/Migrations/20220108174415_removedStatusFromRouteShares.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Api;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace run_it_api.Migrations
 {
     [DbContext(typeof(ApiContext))]
-    partial class ApiContextModelSnapshot : ModelSnapshot
+    [Migration("20220108174415_removedStatusFromRouteShares")]
+    partial class removedStatusFromRouteShares
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ api/Migrations/20220108174415_removedStatusFromRouteShares.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace run_it_api.Migrations
+{
+    public partial class removedStatusFromRouteShares : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "RouteShares");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "RouteShares",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
